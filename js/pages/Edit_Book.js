ad091d78d3d9e0cb95247df6d73324f8aed27199
amrtammer5 <amr.tammer5@gmail.com>
2026-03-24 19:34:05 +0200

    adding Edit.js&Admin Dashboard.js&

---
+++ js/pages/Edit_Book.js
@@ -0,0 +1,37 @@
+window.onload = ()=>{
+let urlParams = new URLSearchParams(window.location.search);
+let editISBN = urlParams.get('edit');
+if (editISBN) {
+
+    const data = localStorage.getItem("Book" + editISBN);
+        
+        if (data) 
+        {
+            const book = JSON.parse(data);
+
+            document.getElementById("Title").value = book.Titel;
+            document.getElementById("Author").value = book.Author;
+            document.getElementById("ISBN").value = book.ISBN;
+            document.getElementById("Publisher").value = book.Publisher;
+            document.getElementById("Publication-Year").value = book.Publication_Year;
+            document.getElementById("Language").value = book.Language;
+            document.getElementById("Category").value = book.Category;
+            document.getElementById("Description").value = book.Description;
+            document.getElementById("Call-Number").value = book.Call_Number;
+            document.getElementById("Total-Copies").value = book.Total_Copies;
+            document.getElementById("Edition").value = book.Edition;
+            document.getElementById("ISBN").readOnly = true;
+
+            let preview = document.getElementById("image-preview");
+            let fileInput = document.getElementById("Cover-Image");
+
+            if (book.Cover_Image) {
+                preview.src = book.Cover_Image;
+                preview.style.display = "block";
+                fileInput.removeAttribute("required"); 
+            }
+            document.querySelector("h1").innerText = "Edit Book: " + book.Titel;
+        }
+
+    }
+}
--- js/pages/admin_dashboard.js
+++ js/pages/admin_dashboard.js
@@ -1,31 +1,63 @@
-window.onload = ()=>{
+window.onload = () => {
     const tableBody = document.querySelector("#Book-Inventory tbody");
-    for(let i = 0;i<localStorage.length;i++)
-    {
-    let key = localStorage.key(i);
-    let data = localStorage.getItem(key);
-    let book = JSON.parse(data);
-    if(book){
-    let row = document.createElement("tr");
+    let Total_Copies =0;
+    let out_of_stock = 0;
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let data = localStorage.getItem(key);
+        let book = JSON.parse(data);
+        if (book) {
+            Total_Copies += Number.parseInt(book.Total_Copies);
+            out_of_stock += Number.parseInt(book.sold);
+            let row = document.createElement("tr");
 
-    let ISBN = document.createElement("td");
-    ISBN.innerText = book.ISBN;
+            let ISBN = document.createElement("td");
+            ISBN.innerText = book.ISBN;
 
-    let titel = document.createElement("td");
-    titel.innerText = book.Titel;
+            let titel = document.createElement("td");
+            titel.innerText = book.Titel;
 
-    let author = document.createElement("td");
-    author.innerText = book.Author;
+            let author = document.createElement("td");
+            author.innerText = book.Author;
 
-    let status = document.createElement("td");
-    status.innerText = (book.Total_Copies == 0) ? "UnAvailable" : "Available";
+            let status = document.createElement("td");
+            status.innerText = (book.Total_Copies == 0) ? "UnAvailable" : "Available";
+            let action = document.createElement("td");
 
-    row.appendChild(ISBN);
-    row.appendChild(titel);
-    row.appendChild(author);
-    row.appendChild(status);
+            let edit = document.createElement("input");
+            edit.type = "button";
+            edit.value = "Edit";
 
-    tableBody.appendChild(row);
+            let delet = document.createElement("input");
+            delet.type = "button";
+            delet.value = "Delete";
+            action.appendChild(edit);
+            action.appendChild(delet);
+
+
+            row.appendChild(ISBN);
+            row.appendChild(titel);
+            row.appendChild(author);
+            row.appendChild(status);
+            row.appendChild(action);
+
+            tableBody.appendChild(row);
+            delet.onclick = () => {
+                delet.parentElement.parentElement.remove();
+                localStorage.removeItem("Book" + book.ISBN)
+            }
+
+
+            edit.onclick = () => {
+                window.location.href = `bookform.html?edit=${book.ISBN}`;
+            };
+
+        }
+
     }
-    }      
+    document.getElementById("Total-Books").innerText = Total_Copies;
+    document.getElementById("Books-Out-of-stock").innerText = out_of_stock;
+    document.getElementById("Available-Books").innerText =Total_Copies - out_of_stock;
+
 }
+
